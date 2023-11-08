6696fdbd027072795aac8ea9a4b9390b7c68ad03
Sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2023-11-08 13:41:18 +0530

    getting started

---
+++ getting_started.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from simple_salesforce import Salesforce, SalesforceLogin, SFType
+import os
+
+# Retrieve Salesforce credentials from GitHub repository secrets
+username = os.environ.get('SALESFORCE_USERNAME')
+password = os.environ.get('SALESFORCE_PASSWORD')
+security_token = os.environ.get('SALESFORCE_SECURITY_TOKEN')
+domain = "login"
+
+session_id, instance = SalesforceLogin(username=username, password=password, security_token=security_token, domain=domain)
+sf = Salesforce(instance=instance, session_id=session_id)
+
+for element in dir(sf):
+    if not element.startswith('_'):
+        if isinstance(getattr(sf, element), str):
+            print('Property Name:{0} ;Value: {1}'.format(element, getattr(sf, element)))
+
+metadata_org = sf.describe()
+print(metadata_org['encoding'])
+print(metadata_org['maxBatchSize'])
+print(metadata_org['sobjects'])
+df_sobjects = pd.DataFrame(metadata_org['sobjects'])
+df_sobjects.to_csv('org metadata info.csv', index=False)
+
+
+# method 2
+account = SFType('account', session_id, instance)
+account_metadata = account.metadata()
+df_account_metadata = pd.DataFrame(account_metadata.get('objectDescribe'))
+df_account_metadata.to_csv('account metadata.csv', index=False)
+
