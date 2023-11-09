bdf636dc312fce9e00d5fbb1a8d35b14a14d9f78
Sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2023-11-09 19:06:33 +0530

    upload metatdata to s3 : test7

--- getting_started.py
+++ getting_started.py
@@ -1,6 +1,8 @@
 import json
 import pandas as pd
 from simple_salesforce import Salesforce, SalesforceLogin, SFType
+import boto3
+import os
    
 sf = Salesforce(username='prakriti.gupta@nucleusteq.com',
                 password='Prakriti@29',
@@ -14,3 +16,16 @@ df = pd.DataFrame(rs['records'])
 print(df)
 df.to_csv('salesforce_data.csv', index=False)
 
+
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+bucket_name = 'aaa-salesforce'
+file_path = 'salesforce_data.csv'
+
+# Create an S3 client
+s3 = boto3.client('s3', aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
+
+# Upload the file
+s3.upload_file(file_path, bucket_name, file_path)
+
+print(f'{file_path} uploaded to {bucket_name} on S3.')
