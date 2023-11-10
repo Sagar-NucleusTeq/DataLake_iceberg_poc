9ab5f77c6bf54a905502c3858744a7f599666ad7
Sagar-NucleusTeq <sagar.kalthiya@nucleusteq.com>
2023-11-11 00:25:43 +0530

    create iceberg table : test2

--- getting_started.py
+++ getting_started.py
@@ -35,58 +35,61 @@ iceberg_table_name = "salesforce_demo"
 aws_access_key_id = os.environ.get("AWS_ACCESS_KEY_ID")
 aws_secret_access_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
 
-file_path = "salesforce_data.csv"
+print(f"{aws_access_key_id}")
+print(f"{aws_secret_access_key}")
 
-# Create an S3 client
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=aws_access_key_id,
-    aws_secret_access_key=aws_secret_access_key,
-)
+# file_path = "salesforce_data.csv"
 
-# Upload the file
-s3.upload_file(file_path, bucket_name, file_path)
+# # Create an S3 client
+# s3 = boto3.client(
+#     "s3",
+#     aws_access_key_id=aws_access_key_id,
+#     aws_secret_access_key=aws_secret_access_key,
+# )
 
-print(f"{file_path} uploaded to {bucket_name} on S3.")
+# # Upload the file
+# s3.upload_file(file_path, bucket_name, file_path)
 
-
-s3_url_file = f"s3://{bucket_name}/{file_path}"
-
-
-df = wr.s3.read_csv(s3_url_file)
-print(df.head())
-print(df.shape)
-print(df.info())
-print("----------columns----------")
-list_of_cols = list(df.columns)
-print(list_of_cols)
+# print(f"{file_path} uploaded to {bucket_name} on S3.")
 
 
-print(f"Reading data from:  ")
-print(f" converted into Pandas DataFrame.")
-
-# Convert all columns to strings
-df = df.astype(str)
-print(f" columns coverted into string.")
+# s3_url_file = f"s3://{bucket_name}/{file_path}"
 
 
-# Cleanup table before create
-wr.catalog.delete_table_if_exists(database=database_name, table=iceberg_table_name)
+# df = wr.s3.read_csv(s3_url_file)
+# print(df.head())
+# print(df.shape)
+# print(df.info())
+# print("----------columns----------")
+# list_of_cols = list(df.columns)
+# print(list_of_cols)
 
-table_location = f"s3://{bucket_name}/1-landing/{iceberg_table_name}"
-temp_table_path = f"s3://{bucket_name}/zzz-temp-dir/{iceberg_table_name}"
 
-print(f"destination_bucket_name : {bucket_name}")
-print(f"database_name : {database_name}")
-print(f"iceberg_table_name : {iceberg_table_name}")
-print(f"table_location : {table_location}")
-print(f"temp_table_path : {temp_table_path}")
+# print(f"Reading data from:  ")
+# print(f" converted into Pandas DataFrame.")
 
-wr.athena.to_iceberg(
-    df=df,
-    database=database_name,
-    table=iceberg_table_name,
-    table_location=table_location,
-    temp_path=temp_table_path,
-)
-print(f"Iceberg table : {iceberg_table_name} : created successfully.")
+# # Convert all columns to strings
+# df = df.astype(str)
+# print(f" columns coverted into string.")
+
+
+# # Cleanup table before create
+# wr.catalog.delete_table_if_exists(database=database_name, table=iceberg_table_name)
+
+# table_location = f"s3://{bucket_name}/1-landing/{iceberg_table_name}"
+# temp_table_path = f"s3://{bucket_name}/zzz-temp-dir/{iceberg_table_name}"
+
+# print(f"destination_bucket_name : {bucket_name}")
+# print(f"database_name : {database_name}")
+# print(f"iceberg_table_name : {iceberg_table_name}")
+# print(f"table_location : {table_location}")
+# print(f"temp_table_path : {temp_table_path}")
+
+# wr.athena.to_iceberg(
+#     df=df,
+#     database=database_name,
+#     table=iceberg_table_name,
+#     table_location=table_location,
+#     temp_path=temp_table_path,
+# )
+# print(f"Iceberg table : {iceberg_table_name} : created successfully.")
